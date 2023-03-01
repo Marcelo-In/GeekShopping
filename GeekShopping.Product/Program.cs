cc726525a776d6c3ac5bd7bf08009e0d138408e5
Marcelo <marceloinoue@hotmail.com>
2023-03-01 11:46:30 -0300

    Criação do model de Product e migration

--- GeekShopping.Product/Program.cs
+++ GeekShopping.Product/Program.cs
@@ -1,4 +1,12 @@
+using GeekShopping.Product.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connection = builder.Configuration.GetConnectionString("MySQLConnection");
+
+builder.Services.AddDbContext<MySQLContext>(options => 
+    options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
 
 // Add services to the container.
 
