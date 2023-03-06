fc4eea2d2a404009ac3c276ec827c56c878ffaff
Marcelo <marceloinoue@hotmail.com>
2023-03-06 12:24:47 -0300

    Criação de projeto com Identity para gerenciar usuários e acessos, adicionadas telas de gerenciamento de Product do serviço Web e integração com serviço ProductAPI.

--- GeekShopping.Product/Controllers/ProductController.cs
+++ GeekShopping.Product/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using GeekShopping.ProductAPI.Data.ValueObjects;
 using GeekShopping.ProductAPI.Repository;
+using GeekShopping.ProductAPI.Utils;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GeekShopping.ProductAPI.Controllers
@@ -16,6 +18,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpGet]
+        [Authorize]
         public async Task<ActionResult<IEnumerable<ProductVO>>> FindAll()
         {
             var products = await _repository.FindAll();
@@ -23,6 +26,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpGet("{id}")]
+        [Authorize]
         public async Task<ActionResult<ProductVO>> FindById(long id)
         {
             var product = await _repository.FindById(id);
@@ -31,7 +35,8 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult<ProductVO>> Create(ProductVO vo)
+        [Authorize]
+        public async Task<ActionResult<ProductVO>> Create([FromBody] ProductVO vo)
         {
             if(vo == null) BadRequest();
             var product = await _repository.Create(vo);
@@ -39,6 +44,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpPut]
+        [Authorize]
         public async Task<ActionResult<ProductVO>> Update(ProductVO vo)
         {
             if (vo == null) BadRequest();
@@ -47,6 +53,7 @@ namespace GeekShopping.ProductAPI.Controllers
         }
 
         [HttpDelete("{id}")]
+        [Authorize(Roles = Role.Admin)]
         public async Task<ActionResult> Delete(long id)
         {
             var status = await _repository.Delete(id);
